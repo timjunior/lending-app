f9ef99e5c6e3c5abc38249e6123ca69a51136501
Timmy Antonio <timmy.antonio@sseenergyservices.com>
2022-10-23 10:12:42 +0100

    added sample api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Holidays from "./component/Holidays";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. <br /> Have updated!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with AWS, YAML and CircleCI Test
-        </a>
-      </header>
+      <Holidays />
     </div>
   );
 }
