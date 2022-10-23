39ebc209d01be9710d476d4e672442e21aa47701
Timmy Antonio <timmy.antonio@sseenergyservices.com>
2022-10-23 10:34:38 +0100

    remove sample api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-import Holidays from "./component/Holidays";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Holidays />
+      <h2>Lending app...</h2>
     </div>
   );
-}
+};
 
 export default App;
