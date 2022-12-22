3096389adce5734e4f42cd30b92e6437d228d833
Timmy Antonio <timmy.antonio@sseenergyservices.com>
2022-12-22 07:11:46 +0000

    update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import axios from "axios";
 import React from "react";
 import "./App.css";
 
 const App = () => {
+  React.useEffect(() => {
+    const callApi = async () => {
+      //return await axios.get("https://ta-lending-api.herokuapp.com/holidays");
+      return axios
+        .post(
+          "https://ta-lending-api.herokuapp.com/products",
+          {
+            name: "egg plant",
+            price: 2.0,
+          },
+          { timeout: 3000 }
+        )
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+
+    callApi().then();
+  }, []);
+
   return (
     <div className="App">
       <h2>Lending app...</h2>
